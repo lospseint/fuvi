1f3e6977c984033844909d62b97f0ad8f813953c
Juan Alarcón <jacadicsa80@yahoo.com>
2022-10-04 09:40:26 -0600

    se agregó de nuevo el archivo constantes.cpp

---
+++ constantes.cpp
@@ -0,0 +1,14 @@
+/*
+nombre del archivo: sumanumeros.cpp
+Autor: Juan Alarcón
+Lugar: ITV
+Instrucciones: Esto solo es una plantilla
+*/
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	
+	return 0;
+}
