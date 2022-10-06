1acb6b5fa58de05945e46d7b1e8e7649044ba2f1
Juan Alarcón <jacadicsa80@yahoo.com>
2022-10-06 08:30:19 -0600

    se agrego el archivo manipuladores.cpp

---
+++ manipuladores.cpp
@@ -0,0 +1,20 @@
+/*
+nombre del archivo: manipuladores.cpp
+autor: juan alarcón
+lugar: ITV
+instrucciones: ejemplos de manipulacion de E/S
+*/
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+
+int main(){
+	cout <<left;
+	cout << setw(20) << "Artista" << setw(10) << "Album" << endl
+<< setw(20)	<< "michael jackson" << setw(10) << "thriller" << endl;
+
+cout << fixed << setprecision (10) << 12.34467;
+	
+	return 0;
+}
---
+++ manipuladores2.cpp
@@ -0,0 +1,30 @@
+/*
+nombre del archivo: manipuladores.cpp
+autor: juan alarcón
+lugar: ITV
+instrucciones: ejemplos de manipulacion de E/S
+*/
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+
+int main(){
+	
+		cout <<left;
+	cout << setw(20) << "Artista" << setw(10) << "Album" << endl
+<< setw(20)	<< "michael jackson" << setw(10) << "thriller" << endl;
+
+cout << "************" << endl;
+
+cout << setw(20) << "asignatura" << setw(10) << "estudiantes" <<endl
+<< setw(20) << "programcion" <<setw(10) << right << 12 << endl
+<< left << setw(20) << "redes" << setw(10) << right << 9 << left << endl;
+
+	
+	
+return 0;	
+	
+	
+	
+}
